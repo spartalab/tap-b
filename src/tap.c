6988cf70fe84fbca1e66cddb706d0a3fd58a9d51
KarthikVelayutham <karthik.velayutham@gmail.com>
2020-08-29 19:02:17 -0500

    Minor changes in conicDer

--- src/tap.c
+++ src/tap.c
@@ -239,9 +239,9 @@ double conicDer(struct arc_type *arc) {
     der += arc->freeFlowTime * (oldRoot_prime + arc->a * x_prime);
 
     /* Add signalized delay */
-    if (arc->flow/arc->saturationFlow <= 0.875) {
+    if (v_s <= 0.875) {
         der += (arc->sParam * v_s_prime )/ ((1 - v_s) * (1 - v_s));
-    } else if (arc->flow/arc->saturationFlow < 0.925) {
+    } else if (v_s < 0.925) {
         der += (arc->CC + v_s *
                  (2 * arc->CB + v_s * 3 *arc->CA)) * v_s_prime;
     }
